826245c6201e343a3994c18f4f131cd3c723666c
bomenakim <96710675+bomena@users.noreply.github.com>
2024-03-15 21:07:24 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Start roscore in the background
-# npm start 프로세스 종료 함수 정의
 cleanup() {
     echo "Stopping npm server..."
     if [ ! -z "$NPM_PID" ]; then
@@ -13,34 +11,26 @@ cleanup() {
         kill $ROSLAUNCH_PID
     fi
 
-    echo "Stopping other background processes..."
-    pkill -P $$  # This will kill all child processes started by this script
+    echo "Stopping Python scripts..."
+    if [ ! -z "$RUN_PID" ]; then
+        kill $RUN_PID
+    fi
 }
-
-# trap 명령으로 스크립트 종료 시 cleanup 함수 실행
 trap cleanup EXIT
 
-# roslaunch를 백그라운드에서 실행하고 PID 저장
 ros2 launch rosbridge_server rosbridge_websocket_launch.xml &
 ROSLAUNCH_PID=$!
-sleep 2  # Give roslaunch time to start
+sleep 2
 
-# run.sh 스크립트 실행
-# 현재 디렉토리를 저장하고 run.sh가 있는 디렉토리로 이동
 CURRENT_DIR=$(pwd)
 cd ./src/function
 
-# run.sh 실행
-./run.sh &
+./python.sh &
 RUN_PID=$!
 
-# 원래 디렉토리로 돌아가기
 cd $CURRENT_DIR
 
-# npm start를 백그라운드에서 실행하고 PID 저장
 npm start &
 NPM_PID=$!
 
-# 스크립트가 종료될 때까지 대기
 wait
-
